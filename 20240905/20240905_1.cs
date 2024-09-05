ceb01b0104412d641bfa95d7d904704de6b0fa42
407_kyousitu <>
2024-09-05 14:48:55 +0900

    20240905

---
+++ 20240905/20240905_1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_
+{
+    class Kart
+    {
+        public int weight;      //重量
+        public int speed;       //速さ
+
+        //アクセルを踏むメソッド
+        public void Force()
+        {
+            Console.WriteLine("加速or減速");
+        }
+
+        //クラクションを鳴らす
+        public virtual void Horn()
+        {
+            Console.WriteLine("ブッブー");
+        }
+    }
+
+    class SkyKart : Kart
+    {
+        //離陸をするメソッド
+        public void Flying()
+        {
+            Console.WriteLine("離陸or着陸");
+        }
+
+        //Hornメソッドをオーバーライド
+        public override void Horn()
+        {
+            Console.WriteLine("ビビッー");
+        }
+    }
+
+    class TurboKart : Kart
+    {
+        public void Trubo()
+        {
+            Console.WriteLine("ブースト！");
+        }
+        public override void Horn()
+        {
+            Console.WriteLine("ビッビー");
+        }
+    }
+
+    internal class _20240905_1
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                //Kart kart = new Kart();
+                //SkyKart skyKart = new SkyKart();
+
+                //kart.Force();
+                //skyKart.Flying();
+
+                ////オーバーライドしたメソッドをそれぞれ呼び出す
+                //kart.Horn();
+                //skyKart.Horn();
+
+                Kart[] karts = new Kart[2];
+
+                //派生クラスのインスタンスを生成
+                karts[0] = new SkyKart();
+                karts[1] = new TurboKart();
+
+                //クラクションメソッドを鳴らします
+                foreach (var kart in karts)
+                {
+                    kart.Horn();
+                }
+            }
+
+        }
+    }
+}
---
+++ 20240905/20240905_Prac01_坂本竜之助.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_
+{
+
+    class Animal
+    {
+        public virtual void Eat()
+        {
+            Console.WriteLine("食べた");
+        }
+    }
+
+    class Lion : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("肉を食べた");
+        }
+    }
+
+    class Elephant : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("草を食べた");
+        }
+    }
+
+
+
+    internal class _20240905_Prac01_坂本竜之助
+    {
+
+        static void Main(string[] args)
+        {
+            //インスタンス化
+            Animal[] animals = new Animal[2]; 
+
+            //派生クラスのインスタンス化
+            animals[0] = new Lion();
+            animals[1] = new Elephant();
+
+            foreach (var a in animals)
+            {
+                a.Eat();
+            }
+        }
+
+    }
+}

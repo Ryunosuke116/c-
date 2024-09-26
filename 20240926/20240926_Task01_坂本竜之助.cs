3c9b29de029cf96ef5a0416ed65db59b25eaf62d
407-06\student <Ytappii175773@edu.fca.ac.jp>
2024-09-26 17:25:41 +0900

    20240926

---
+++ 20240926/20240926_Task01_坂本竜之助.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_._20240926
+{
+    public class Player
+    {
+        protected int number;
+
+        public int GetNumber()
+        {
+            return number;
+        }
+      
+        //インスタンス化
+        public Player()
+        {
+            number = 0;
+        }
+        public void choice()
+        {
+            while (true)
+            {
+
+                Console.WriteLine("0：ストレート　1：カーブ　2：スライダー　3：シンカーの中から選んでください");
+                number = int.Parse(Console.ReadLine());
+
+                //0以上3以下の場合次に行く
+                if(number >= 0 && number <= 3)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("入力し直してください");
+                }
+            }
+        }
+    }
+
+    public class CPU : Player
+    {
+        //インスタンス化
+        public CPU()
+        {
+            number = 0;
+        }
+
+        public void choice()
+        {
+            Random random = new Random();
+            number = random.Next(0,4);
+        }
+    }
+
+    public class Judge
+    {
+        private int ballCount;
+        private int strikeCount;
+        private int outCount;
+        private int hitCount;
+        private bool isEnd;
+
+        public bool GetIsEnd()
+        {
+            return isEnd;
+        }
+
+        //初期化
+        public Judge()
+        {
+            ballCount = 0;
+            strikeCount = 0;
+            outCount = 0;
+            hitCount = 0;
+            isEnd = false;
+        }
+        public void judge(int playerNumber,int CpuNumber)
+        {
+            //数字が違う場合ストライクorボール
+            if (playerNumber != CpuNumber)
+            {
+                Random random = new Random();
+                int num = random.Next(0, 4);
+
+                if (num >= 0 && num <= 2)
+                {
+                    Console.WriteLine("ストライク！！");
+                    //75%の確率でストライク、25%でボール
+                    strikeCount++;
+                    //ストライク3つで1アウト
+                    if (strikeCount >= 3)
+                    {
+                        Console.WriteLine("アウト！");
+                        outCount++;
+                        strikeCount = 0;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ボール！");
+                    ballCount++;
+                    //ボール４つで1ヒット
+                    if (ballCount >= 4)
+                    {
+                        hitCount++;
+                    }
+                }
+            //同じ数字でヒット
+            }
+            else if (playerNumber == CpuNumber)
+            {
+                Console.WriteLine("ヒット！");
+                hitCount++;
+            }
+            
+            //3アウトで試合終了、プレイヤーの勝利
+            if(outCount >= 3)
+            {
+                Console.WriteLine("試合終了！！");
+                Console.WriteLine("PLAYER WIN");
+                isEnd = true;
+            }
+            else if(hitCount >= 4)
+            {
+                Console.WriteLine("試合終了！！");
+                Console.WriteLine("CPU WIN");
+                isEnd = true;
+            }
+        }
+    }
+
+
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            CPU cpu = new CPU();
+            Judge judge = new Judge();
+
+            while (true)
+            {
+                player.choice();
+                cpu.choice();
+                judge.judge(player.GetNumber(),cpu.GetNumber());
+
+                if(judge.GetIsEnd())
+                {
+                    break;
+                }
+            }    
+        }
+
+    }
+}
---
+++ 20240926/20240926_Task02_坂本竜之助.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_._20240926
+{
+    public class Player
+    {
+        protected int number;
+        protected int isDraw;
+
+        public Player() 
+        {
+            number = 0;
+            isDraw = 0;
+        }
+
+        public int Number
+        {
+            get { return this.number; }
+            set { this.number = value; }
+        }
+        public int IsDraw
+        {
+            get { return this.isDraw; }
+            set { this.isDraw = value; }
+        }
+        
+        //カードを引く
+        public void CardDraw()
+        {
+            Random random = new Random();
+            Number += random.Next(1, 12);
+        }
+
+        public void isCardDraw()
+        {
+            Console.WriteLine("カードを引きますか？");
+            Console.WriteLine("Yes:0 No:1");
+            while (true)
+            {
+                isDraw = int.Parse(Console.ReadLine());
+                if (isDraw >= 0 && isDraw <= 1)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("書き直してください");
+                }
+            }
+        }
+    }
+
+    public class Enemy:Player
+    {
+        public Enemy()
+        {
+            number = 0;
+            isDraw = 0;
+        }
+    }
+
+    public class Judge
+    {
+        private Player player;
+        private Enemy enemy;
+        public Judge()
+        {
+           this.player = new Player();
+           this.enemy = new Enemy();
+        }
+
+        public void DrawCard()
+        {
+            Random random = new Random();
+
+            //カードを二回引く
+            for (int i = 0; i < 2; i++)
+            {
+                player.Number = random.Next(1, 12);
+                enemy.Number = random.Next(1, 12);
+            }
+            Display();
+        }
+        
+        public void Display()
+        {
+            Console.WriteLine($"プレイヤー:{player.Number}\n相手:{enemy.Number}");
+        }
+        public void judge()
+        {
+            //初期カードを引く
+            DrawCard();
+
+            while (true)
+            {
+                //カードを引くか決める
+                player.isCardDraw();
+
+                if (player.IsDraw == 0)
+                {
+                    player.CardDraw();
+                }
+                //ジャッジ
+                if(player.Number == 21)
+                {
+                    Display();
+                    Console.WriteLine("プレイヤーの勝ち");
+                    break;
+                }
+                else if(player.Number >= 22)
+                {
+                    Display();
+                    Console.WriteLine("プレイヤーの負け");
+                    break;
+                }
+                //プレイヤーよりカードの数字が小さい場合カードを引く
+                if(player.Number >= enemy.Number)
+                {
+                    enemy.CardDraw();
+                }
+                //ジャッジ
+                if (enemy.Number == 21)
+                {
+                    Display();
+                    Console.WriteLine("プレイヤーの負け");
+                    break;
+                }
+                else if (enemy.Number >= 22)
+                {
+                    Display();
+                    Console.WriteLine("プレイヤーの勝ち");
+                    break;
+                }
+                //現在のカードを表示
+                Display();
+            }
+        }
+    }
+
+
+    internal class _20240926_Task02_坂本竜之助
+    {
+        static void Main(string[] args)
+        {
+            Judge judge = new Judge();
+            judge.judge();
+        }
+    }
+}
